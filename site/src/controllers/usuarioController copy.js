04cf28f12fe55260001cdef1d2775199d0a1e494
Rebeca <rebeca.ferreira@sptech.school>
2025-06-09 18:00:10 -0300

    criação da página index finalizada

--- site/src/controllers/usuarioController copy.js
+++ site/src/controllers/usuarioController copy.js
@@ -54,10 +54,10 @@ function autenticar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
-    // var cpf = req.body.cpfServer;
+    var cpf = req.body.cpfServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    // var fkEmpresa = req.body.idEmpresaVincularServer;
+    var fkEmpresa = req.body.idEmpresaVincularServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -66,10 +66,10 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    // } else if (fkEmpresa == undefined) {
-    //     res.status(400).send("Sua empresa a vincular está undefined!");
-    // } else if (cpf == undefined) {
-    //     res.status(400).send("Seu cpf está undefined!");
+    } else if (fkEmpresa == undefined) {
+        res.status(400).send("Sua empresa a vincular está undefined!");
+    } else if (cpf == undefined) {
+        res.status(400).send("Seu cpf está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
